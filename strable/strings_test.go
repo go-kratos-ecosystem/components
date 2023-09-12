1f4dc915abf23a44d6107a572a56f40505b3eb90
Flc゛ <four_leaf_clover@foxmail.com>
2023-09-12 12:48:40 +0800

    feat: add `strable` (#8)

---
+++ strable/strings_test.go
@@ -0,0 +1,70 @@
+package strable
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIs(t *testing.T) {
+	assert.True(t, Is("abc", "abc"))
+	assert.False(t, Is("abcc", "abc"))
+	assert.True(t, Is("ab*", "abc"))
+	assert.True(t, Is("ab*", "ab"))
+	assert.True(t, Is("ab/*", "ab/cc"))
+	assert.True(t, Is("ab/*", "ab/"))
+	assert.True(t, Is("*", "ab"))
+	assert.True(t, Is("*", ""))
+	assert.True(t, Is("ab/*", "ab/cc/dd"))
+	assert.True(t, Is("*dd/", "ab/cc/dd/"))
+	assert.False(t, Is("*dd/d", "dd/"))
+}
+
+func TestInArray(t *testing.T) {
+	assert.True(t, InArray("1", []string{"1", "2"}))
+	assert.True(t, InArray("2", []string{"1", "2"}))
+	assert.False(t, InArray("3", []string{"1", "2"}))
+	assert.False(t, InArray("12", []string{"1", "2"}))
+}
+
+func TestMd5(t *testing.T) {
+	assert.Equal(t, "900150983cd24fb0d6963f7d28e17f72", Md5("abc"))
+}
+
+func TestSha1(t *testing.T) {
+	assert.Equal(t, "a9993e364706816aba3e25717850c26c9cd0d89d", Sha1("abc"))
+}
+
+func TestReverse(t *testing.T) {
+	assert.Equal(t, "cba", Reverse("abc"))
+}
+
+func TestReplace(t *testing.T) {
+	assert.Equal(t, "bbbbcc", Replace("aabbcc", "a", "b"))
+}
+
+func TestStrShuffle(t *testing.T) {
+	assert.True(t, InArray(Shuffle("abc"), []string{"abc", "acb", "bac", "bca", "cab", "cba"}))
+}
+
+func TestRandom(t *testing.T) {
+	r1, r2 := Random(10), Random(10)
+
+	assert.Equal(t, 10, len(r1))
+	assert.Equal(t, 10, len(r2))
+	assert.NotEqual(t, r1, r2)
+}
+
+func TestLength(t *testing.T) {
+	assert.Equal(t, 3, Len("abc"))
+	assert.Equal(t, 5, Len("^*&%*"))
+	assert.Equal(t, 4, Len("张三李四"))
+
+	assert.Equal(t, 12, len("张三李四"))
+}
+
+func TestIsUuid(t *testing.T) {
+	assert.True(t, IsUuid("f81d4fae-7dec-11d0-a765-00a0c91e6bf6"))
+	assert.False(t, IsUuid("f81d4fae-7dec-11d0-a765-00a0c91e6bf"))
+	assert.False(t, IsUuid("f81d4fae-7dec-11d0-a765-00a0c91e6bf6a"))
+}
