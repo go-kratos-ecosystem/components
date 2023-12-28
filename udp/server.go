83c6201f31160fbbe2d1cef03e40ddb9b82f6165
Flc゛ <four_leaf_clover@foxmail.com>
2023-12-28 23:31:20 +0800

    feat(udp): Added udp server

--- udp/server.go
+++ udp/server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"net"
+	"sync"
 )
 
 type Server struct {
@@ -12,6 +13,7 @@ type Server struct {
 	bufSize int
 
 	conn net.PacketConn
+	mu   sync.Mutex // guards conn
 
 	handler func(conn net.PacketConn, buf []byte, addr net.Addr)
 
@@ -84,6 +86,9 @@ func (s *Server) Start(ctx context.Context) (err error) {
 }
 
 func (s *Server) handle(buf []byte, addr net.Addr) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	if s.recoveryHandler != nil {
 		defer func() {
 			if err := recover(); err != nil {
@@ -96,6 +101,9 @@ func (s *Server) handle(buf []byte, addr net.Addr) {
 }
 
 func (s *Server) Stop(ctx context.Context) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	log.Println("udp server: stopping")
 
 	return s.conn.Close()
