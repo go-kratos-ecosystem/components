0cac3a3ff75c2a7e88a3b072045f06bb33e114d7
Flc゛ <four_leaf_clover@foxmail.com>
2023-12-28 23:07:10 +0800

    feat(udp): Added udp server

---
+++ udp/server.go
@@ -0,0 +1,102 @@
+package udp
+
+import (
+	"context"
+	"log"
+	"net"
+)
+
+type Server struct {
+	address string
+
+	bufSize int
+
+	conn net.PacketConn
+
+	handler func(conn net.PacketConn, buf []byte, addr net.Addr)
+
+	recoveryHandler func(conn net.PacketConn, buf []byte, addr net.Addr, err interface{})
+}
+
+type Option func(*Server)
+
+func WithBufSize(bufSize int) Option {
+	return func(s *Server) {
+		if bufSize > 0 {
+			s.bufSize = bufSize
+		}
+	}
+}
+
+func WithHandler(handler func(conn net.PacketConn, buf []byte, addr net.Addr)) Option {
+	return func(s *Server) {
+		if handler != nil {
+			s.handler = handler
+		}
+	}
+}
+
+func WithRecoveryHandler(handler func(conn net.PacketConn, buf []byte, addr net.Addr, err interface{})) Option {
+	return func(s *Server) {
+		if handler != nil {
+			s.recoveryHandler = handler
+		}
+	}
+}
+
+func NewServer(address string, opts ...Option) *Server {
+	s := &Server{
+		address: address,
+		bufSize: 1024,
+	}
+
+	for _, opt := range opts {
+		opt(s)
+	}
+
+	return s
+}
+
+func (s *Server) Start(ctx context.Context) (err error) {
+	s.conn, err = net.ListenPacket("udp", s.address)
+	if err != nil {
+		return
+	}
+
+	log.Printf("udp server: listening on %s\n", s.address)
+
+	buf := make([]byte, s.bufSize)
+
+	for {
+		n, addr, err := s.conn.ReadFrom(buf)
+		if err != nil {
+			return err
+		}
+
+		if s.handler == nil {
+			log.Printf("udp server: receive from %s: %s\n", addr.String(), string(buf))
+			continue
+		}
+
+		go s.handle(buf[:n], addr)
+	}
+
+}
+
+func (s *Server) handle(buf []byte, addr net.Addr) {
+	if s.recoveryHandler != nil {
+		defer func() {
+			if err := recover(); err != nil {
+				s.recoveryHandler(s.conn, buf, addr, err)
+			}
+		}()
+	}
+
+	s.handler(s.conn, buf, addr)
+}
+
+func (s *Server) Stop(ctx context.Context) error {
+	log.Println("udp server: stopping")
+
+	return s.conn.Close()
+}
