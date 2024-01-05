1c9aea243e7df654c6b4ec5897d1423041f8d504
Flc゛ <four_leaf_clover@foxmail.com>
2024-01-05 18:42:54 +0800

    refactor(recovery): Optimized recovery (#53)

--- recovery/recovery.go
+++ recovery/recovery.go
@@ -1,47 +1,39 @@
 package recovery
 
-type options struct {
+type Recovery struct {
 	handler func(err interface{})
 }
 
-type Option func(o *options)
+type Option func(o *Recovery)
 
 func WithHandler(handler func(err interface{})) Option {
-	return func(o *options) {
-		o.handler = handler
+	return func(r *Recovery) {
+		r.handler = handler
 	}
-}
-
-type Recovery struct {
-	opt *options
 }
 
 func New(opts ...Option) *Recovery {
-	o := &options{}
+	r := &Recovery{
+		handler: func(err interface{}) {
+			panic(err)
+		},
+	}
 
 	for _, opt := range opts {
-		opt(o)
+		opt(r)
 	}
 
-	if o.handler == nil {
-		o.handler = func(err interface{}) {
-			panic(err)
-		}
-	}
-
-	return &Recovery{
-		opt: o,
-	}
+	return r
 }
 
 func (r *Recovery) Wrap(f func()) {
-	defer func() {
-		if err := recover(); err != nil {
-			if r.opt.handler != nil {
-				r.opt.handler(err)
+	if r.handler != nil {
+		defer func() {
+			if err := recover(); err != nil {
+				r.handler(err)
 			}
-		}
-	}()
+		}()
+	}
 
 	f()
 }
