622ddfa83b957d0ddf7e72cbf77cde620598da29
Flc゛ <four_leaf_clover@foxmail.com>
2024-01-22 23:23:43 +0800

    refactor(log): Refactored manager logger to logging library. (#72)
    * refactor(log): Refactored manager logger to logging library.
    
    Signed-off-by: Flc゛ <four_leaf_clover@foxmail.com>
    
    * refactor(log): Refactored manager logger to logging library.
    
    Signed-off-by: Flc゛ <four_leaf_clover@foxmail.com>
    
    * refactor(log): Refactored manager logger to logging library.
    
    Signed-off-by: Flc゛ <four_leaf_clover@foxmail.com>
    
    * refactor(log): Refactored manager logger to logging library.
    
    Signed-off-by: Flc゛ <four_leaf_clover@foxmail.com>
    
    ---------
    
    Signed-off-by: Flc゛ <four_leaf_clover@foxmail.com>

---
+++ log/logging/logger.go
@@ -0,0 +1,48 @@
+package logging
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/go-kratos/kratos/v2/log"
+)
+
+var ErrNoLogger = errors.New("logging: the logger is not defined")
+
+type Logger struct {
+	log.Logger
+	loggers map[string]log.Logger
+}
+
+func New(logger log.Logger) *Logger {
+	return &Logger{
+		Logger:  logger,
+		loggers: make(map[string]log.Logger),
+	}
+}
+
+func (l *Logger) Log(level log.Level, keyvals ...interface{}) error {
+	if l.Logger != nil {
+		return l.Logger.Log(level, keyvals...)
+	}
+
+	return ErrNoLogger
+}
+
+func (l *Logger) Register(name string, logger log.Logger) {
+	l.loggers[name] = logger
+}
+
+func (l *Logger) Channel(names ...string) log.Logger {
+	if len(names) <= 0 {
+		return l.Logger
+	}
+
+	name := names[0]
+
+	if logger, ok := l.loggers[name]; ok {
+		return logger
+	}
+
+	panic(fmt.Errorf("logging: unknown logger %s", name))
+}
---
+++ log/logging/logger_test.go
@@ -0,0 +1,31 @@
+package logging
+
+import (
+	"testing"
+	"time"
+
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestManager_Channel(t *testing.T) {
+	logger := New(log.DefaultLogger)
+	logger.Register("ts", log.With(
+		log.DefaultLogger,
+		"ts", log.Timestamp(time.RFC3339),
+	))
+
+	logger.Log(log.LevelDebug, "test", "test")               //nolint:errcheck
+	logger.Channel().Log(log.LevelDebug, "test", "test")     //nolint:errcheck
+	logger.Channel("ts").Log(log.LevelDebug, "test", "test") //nolint:errcheck
+
+	assert.Panics(t, func() {
+		logger.Channel("unknown").Log(log.LevelDebug, "test", "test") //nolint:errcheck
+	})
+}
+
+func TestManager_Log(t *testing.T) {
+	m := New(nil)
+
+	assert.EqualError(t, m.Log(log.LevelDebug, "test", "test"), ErrNoLogger.Error())
+}
