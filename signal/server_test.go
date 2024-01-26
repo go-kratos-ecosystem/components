8011549999a631f58bba786809a8d4ad523a3d81
Flc゛ <four_leaf_clover@foxmail.com>
2024-01-26 09:39:57 +0800

    feat(signal): Added Signal Server (#76)
    * feat(signal): Added Signal Server
    
    Signed-off-by: Flc゛ <four_leaf_clover@foxmail.com>
    
    * feat(signal): Added Signal Server
    
    Signed-off-by: Flc゛ <four_leaf_clover@foxmail.com>
    
    * feat(signal): Added Signal Server
    
    Signed-off-by: Flc゛ <four_leaf_clover@foxmail.com>
    
    * feat(signal): Added Signal Server
    
    Signed-off-by: Flc゛ <four_leaf_clover@foxmail.com>
    
    ---------
    
    Signed-off-by: Flc゛ <four_leaf_clover@foxmail.com>

---
+++ signal/server_test.go
@@ -0,0 +1,77 @@
+package signal
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os"
+	"sync"
+	"syscall"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	buffer bytes.Buffer
+	mu     sync.Mutex
+)
+
+func TestServer(t *testing.T) {
+	srv := newServer()
+
+	go srv.Start(context.Background()) //nolint:errcheck
+
+	time.Sleep(2 * time.Second)
+
+	assert.NoError(t, syscall.Kill(os.Getpid(), syscall.SIGUSR1))
+	assert.NoError(t, syscall.Kill(os.Getpid(), syscall.SIGUSR2))
+
+	time.Sleep(2 * time.Second)
+
+	mu.Lock()
+	assert.Equal(t, `exampleHandler signal: user defined signal 1
+signal: user defined signal 1, handler: *signal.example2Handler, err: example2Handler panic
+exampleHandler signal: user defined signal 2
+`, buffer.String())
+	mu.Unlock()
+
+	srv.Stop(context.Background()) //nolint:errcheck
+}
+
+func newServer() *Server {
+	srv := NewServer(
+		WithRecoveryHandler(func(err interface{}, signal os.Signal, handler Handler) {
+			mu.Lock()
+			defer mu.Unlock()
+			buffer.WriteString(fmt.Sprintf("signal: %s, handler: %T, err: %v\n", signal, handler, err))
+		}),
+	)
+
+	srv.Register(&exampleHandler{}, &example2Handler{})
+
+	return srv
+}
+
+type exampleHandler struct{}
+
+func (h *exampleHandler) Listen() []os.Signal {
+	return []os.Signal{syscall.SIGUSR1, syscall.SIGUSR2}
+}
+
+func (h *exampleHandler) Handle(sig os.Signal) {
+	mu.Lock()
+	defer mu.Unlock()
+	buffer.WriteString(fmt.Sprintf("exampleHandler signal: %s\n", sig))
+}
+
+type example2Handler struct{}
+
+func (h *example2Handler) Listen() []os.Signal {
+	return []os.Signal{syscall.SIGUSR1}
+}
+
+func (h *example2Handler) Handle(os.Signal) {
+	panic("example2Handler panic")
+}
