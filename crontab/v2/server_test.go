3a46d53a3e2897ce9ce6b7e6949d2a7aeff312e6
Flc゛ <four_leaf_clover@foxmail.com>
2024-02-14 22:12:55 +0800

    refactor(crontab/v2): Refactor v2 version (#95)
    * refactor(crontab/v2): Refactor v2 version
    
    Signed-off-by: Flc゛ <four_leaf_clover@foxmail.com>
    
    * refactor(crontab/v2): Refactor v2 version
    
    Signed-off-by: Flc゛ <four_leaf_clover@foxmail.com>
    
    ---------
    
    Signed-off-by: Flc゛ <four_leaf_clover@foxmail.com>

---
+++ crontab/v2/server_test.go
@@ -0,0 +1,55 @@
+package v2
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/robfig/cron/v3"
+)
+
+type mockJob struct{}
+
+func RegisterMockJob(srv *Server) {
+	n := newMockJob()
+	_, _ = srv.AddJob(n.Exp(), n)
+}
+
+func newMockJob() *mockJob {
+	return &mockJob{}
+}
+
+func (j *mockJob) Exp() string {
+	return "* * * * * *"
+}
+
+func (j *mockJob) Run() {
+	data <- "Hello World!"
+}
+
+var (
+	ctx  = context.Background()
+	data = make(chan string, 1)
+)
+
+func TestCrontab(t *testing.T) {
+	srv := NewServer(cron.New(
+		cron.WithSeconds(),
+	))
+
+	RegisterMockJob(srv)
+
+	go srv.Start(ctx)   //nolint:errcheck
+	defer srv.Stop(ctx) //nolint:errcheck
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second*2)
+	defer cancel()
+
+	select {
+	case <-ctx.Done():
+		t.Error("Crontab test timeout")
+		return
+	case msg := <-data:
+		t.Log(msg)
+	}
+}
