2bc8a3a394b839eb4bde05dddfe8f2b856f20b1a
Flc゛ <four_leaf_clover@foxmail.com>
2024-02-22 12:55:11 +0800

    test(gin): Improve unit tests (#115)
    Signed-off-by: Flc゛ <four_leaf_clover@foxmail.com>

---
+++ gin/server_test.go
@@ -0,0 +1,49 @@
+package gin
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"sync"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestServer(t *testing.T) {
+	var (
+		srv = NewServer(
+			gin.New(),
+			WithAddr(":8080"),
+		)
+		ch = make(chan string, 1)
+		wg sync.WaitGroup
+	)
+	wg.Add(1)
+
+	srv.GET("/ping", func(c *gin.Context) {
+		defer wg.Done()
+		ch <- "pong"
+		c.String(200, "pong")
+	})
+
+	go func() {
+		srv.Start(context.Background()) // nolint: errcheck
+	}()
+
+	resp, err := http.Get("http://localhost:8080/ping")
+
+	wg.Wait()
+
+	assert.NoError(t, err)
+	assert.Equal(t, 200, resp.StatusCode)
+	defer resp.Body.Close() // nolint: errcheck
+	body, _ := io.ReadAll(resp.Body)
+	assert.Equal(t, "pong", string(body))
+
+	assert.Equal(t, "pong", <-ch)
+
+	err = srv.Stop(context.Background())
+	assert.NoError(t, err)
+}
