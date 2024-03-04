c2c42f1ce2da786fa62be62944b64c9340fdb47b
Flc゛ <four_leaf_clover@foxmail.com>
2024-03-04 16:14:37 +0800

    refactor(debug): Refactor interface to any
    Signed-off-by: Flc゛ <four_leaf_clover@foxmail.com>

--- debug/dump.go
+++ debug/dump.go
@@ -9,20 +9,20 @@ import (
 // Dump dumps the given values.
 // It is a wrapper for spew.Dump(https://github.com/davecgh/go-spew)
 // This is useful for debugging.
-func Dump(v ...interface{}) {
+func Dump(v ...any) {
 	spew.Dump(v...)
 }
 
 // Sdump the given values and returns the result.
 // It's a wrapper for spew.Sdump(https://github.com/davecgh/go-spew)
 // This is useful for debugging.
-func Sdump(v ...interface{}) string {
+func Sdump(v ...any) string {
 	return spew.Sdump(v...)
 }
 
 // Fdump the given values to the given writer.
 // It's a wrapper for spew.Fdump(https://github.com/davecgh/go-spew)
 // This is useful for debugging.
-func Fdump(w io.Writer, v ...interface{}) {
+func Fdump(w io.Writer, v ...any) {
 	spew.Fdump(w, v...)
 }
