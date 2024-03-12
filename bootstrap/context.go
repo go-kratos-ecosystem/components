6f4d5cf260d558bbf4a6272b2102f5b1370a5001
Flc゛ <four_leaf_clover@foxmail.com>
2024-03-12 23:20:51 +0800

    refactor(bootstrap): refactor Context
    Signed-off-by: Flc゛ <four_leaf_clover@foxmail.com>

--- bootstrap/context.go
+++ bootstrap/context.go
@@ -6,53 +6,14 @@ import (
 
 type Provider func(ctx context.Context) (context.Context, error)
 
-type Context struct {
-	context.Context
-	providers []Provider
-}
-
-type ContextOption func(*Context)
-
-func WithContext(ctx context.Context) ContextOption {
-	return func(c *Context) {
-		c.Context = ctx
-	}
-}
-
-func WithProviders(providers ...Provider) ContextOption {
-	return func(c *Context) {
-		c.Register(providers...)
-	}
-}
-
-func NewContext(opts ...ContextOption) *Context {
-	ctx := &Context{}
-
-	for _, opt := range opts {
-		opt(ctx)
-	}
-
-	ctx.init()
-
-	return ctx
-}
-
-func (c *Context) init() {
-	if c.Context == nil {
-		c.Context = context.Background()
-	}
-}
-
-func (c *Context) Register(providers ...Provider) {
-	c.providers = append(c.providers, providers...)
-}
-
-func (c *Context) Boot() (ctx context.Context, err error) {
-	ctx = c.Context
-	for _, provider := range c.providers {
-		if ctx, err = provider(ctx); err != nil {
-			return
+func NewContext(ctx context.Context, providers ...Provider) (context.Context, error) {
+	var err error
+	for _, provider := range providers {
+		if provider != nil {
+			if ctx, err = provider(ctx); err != nil {
+				return ctx, err
+			}
 		}
 	}
-	return
+	return ctx, nil
 }
