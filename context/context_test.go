ca881d1c5a3ad0dd0422272a1c92ab19762f3071
Flc゛ <four_leaf_clover@foxmail.com>
2024-03-14 22:22:42 +0800

    refactor(context): Rename `Chain` to `Pipe` and add `Chain`
    Signed-off-by: Flc゛ <four_leaf_clover@foxmail.com>

--- context/context_test.go
+++ context/context_test.go
@@ -8,36 +8,43 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestContext(t *testing.T) {
-	type (
-		mockProviderStruct1 struct{}
-		mockProviderStruct2 struct{}
-		mockProviderStruct3 struct{}
-	)
+type (
+	mockProviderStruct1 struct{}
+	mockProviderStruct2 struct{}
+	mockProviderStruct3 struct{}
+)
 
-	var (
-		mockProvider1 = func(ctx context.Context) (context.Context, error) {
-			return context.WithValue(ctx, mockProviderStruct1{}, "mockProvider1"), nil
-		}
+var result = make(chan string, 3)
 
-		mockProvider2 = func(ctx context.Context) (context.Context, error) {
-			return context.WithValue(ctx, mockProviderStruct2{}, "mockProvider2"), nil
-		}
+var (
+	mockProvider1 = func(ctx context.Context) (context.Context, error) {
+		result <- "mockProvider1"
+		return context.WithValue(ctx, mockProviderStruct1{}, "mockProvider1"), nil
+	}
 
-		mockProvider3 = func(ctx context.Context) (context.Context, error) {
-			return ctx, errors.New("mockProvider3")
-		}
-	)
+	mockProvider2 = func(ctx context.Context) (context.Context, error) {
+		result <- "mockProvider2"
+		return context.WithValue(ctx, mockProviderStruct2{}, "mockProvider2"), nil
+	}
 
-	ctx1, err1 := Chain(
+	mockProvider3 = func(ctx context.Context) (context.Context, error) {
+		result <- "mockProvider3"
+		return ctx, errors.New("mockProvider3")
+	}
+)
+
+func TestPipe(t *testing.T) {
+	ctx1, err1 := Pipe(
 		context.Background(),
 		mockProvider1, mockProvider2,
 	)
 	assert.NoError(t, err1)
 	assert.Equal(t, "mockProvider1", ctx1.Value(mockProviderStruct1{}))
 	assert.Equal(t, "mockProvider2", ctx1.Value(mockProviderStruct2{}))
+	assert.Equal(t, "mockProvider1", <-result)
+	assert.Equal(t, "mockProvider2", <-result)
 
-	ctx2, err2 := Chain(
+	ctx2, err2 := Pipe(
 		context.Background(),
 		mockProvider1, mockProvider3,
 	)
@@ -45,4 +52,25 @@ func TestContext(t *testing.T) {
 	assert.NotNil(t, ctx2)
 	assert.Equal(t, "mockProvider1", ctx2.Value(mockProviderStruct1{}))
 	assert.Nil(t, ctx2.Value(mockProviderStruct3{}))
+	assert.Equal(t, "mockProvider1", <-result)
+}
+
+func TestChain(t *testing.T) {
+	ctx1, err1 := Chain(
+		context.Background(),
+		mockProvider1, mockProvider2,
+	)
+	assert.NoError(t, err1)
+	assert.Equal(t, "mockProvider1", ctx1.Value(mockProviderStruct1{}))
+	assert.Equal(t, "mockProvider2", ctx1.Value(mockProviderStruct2{}))
+	assert.Equal(t, "mockProvider2", <-result)
+	assert.Equal(t, "mockProvider1", <-result)
+
+	ctx2, err2 := Chain(
+		context.Background(),
+		mockProvider3, mockProvider1,
+	)
+	assert.Error(t, err2)
+	assert.Equal(t, "mockProvider1", ctx2.Value(mockProviderStruct1{}))
+	assert.Equal(t, "mockProvider1", <-result)
 }
