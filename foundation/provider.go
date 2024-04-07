fed5408021892e07960549990a6cf12a872ef592
Flc゛ <four_leaf_clover@foxmail.com>
2024-04-07 19:11:50 +0800

    feat(foundation): Added `BootstrapFunc` and `TerminateFunc` (#208)
    Signed-off-by: Flc゛ <four_leaf_clover@foxmail.com>

--- foundation/provider.go
+++ foundation/provider.go
@@ -16,3 +16,23 @@ func (*BaseProvider) Bootstrap(ctx context.Context) (context.Context, error) {
 func (*BaseProvider) Terminate(ctx context.Context) (context.Context, error) {
 	return ctx, nil
 }
+
+type BootstrapFunc func(context.Context) (context.Context, error)
+
+func (f BootstrapFunc) Bootstrap(ctx context.Context) (context.Context, error) {
+	return f(ctx)
+}
+
+func (f BootstrapFunc) Terminate(ctx context.Context) (context.Context, error) {
+	return ctx, nil
+}
+
+type TerminateFunc func(context.Context) (context.Context, error)
+
+func (f TerminateFunc) Bootstrap(ctx context.Context) (context.Context, error) {
+	return ctx, nil
+}
+
+func (f TerminateFunc) Terminate(ctx context.Context) (context.Context, error) {
+	return f(ctx)
+}
