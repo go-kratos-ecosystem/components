d5daf5a92afabb4e44ea85c0eb06557a3497eafe
Flc゛ <four_leaf_clover@foxmail.com>
2024-07-10 11:52:52 +0800

    feat(locker): Added locker
    Signed-off-by: Flc゛ <four_leaf_clover@foxmail.com>

---
+++ locker/memory/locker_test.go
@@ -0,0 +1,41 @@
+package memory
+
+import (
+	"context"
+	"errors"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/go-kratos-ecosystem/components/v2/locker"
+)
+
+func TestLocker(t *testing.T) {
+	var (
+		wg   sync.WaitGroup
+		errs []error
+		l    = NewLocker(5 * time.Second)
+		ctx  = context.Background()
+	)
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			err := l.Try(ctx, func() error {
+				time.Sleep(500 * time.Millisecond)
+				return nil
+			})
+			if err != nil && errors.Is(err, locker.ErrLocked) {
+				errs = append(errs, err)
+			}
+		}()
+	}
+
+	wg.Wait()
+	assert.Truef(t, len(errs) > 0, "expect error, got nil")
+
+	assert.NotEmpty(t, l.Owner(ctx))
+}
