6284161e91f42d18d88d9d43ac8cb61317567f13
Flc゛ <four_leaf_clover@foxmail.com>
2024-08-14 17:45:38 +0800

    feat(hyperf/jet/middleware): added recovery middleware
    Signed-off-by: Flc゛ <four_leaf_clover@foxmail.com>

---
+++ hyperf/jet/middleware/recovery/recovery.go
@@ -0,0 +1,46 @@
+package recovery
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-kratos-ecosystem/components/v2/hyperf/jet"
+)
+
+var DefaultHandler = func(ctx context.Context, name string, request any, err any) error {
+	return fmt.Errorf("name: %s, request: %v, error: %v", name, request, err)
+}
+
+type HandlerFunc func(ctx context.Context, name string, request any, err any) error
+
+type options struct {
+	handler HandlerFunc
+}
+
+type Option func(*options)
+
+func Handler(h HandlerFunc) Option {
+	return func(o *options) {
+		o.handler = h
+	}
+}
+
+func NewRecovery(opts ...Option) jet.Middleware {
+	return func(next jet.Handler) jet.Handler {
+		o := options{
+			handler: DefaultHandler,
+		}
+		for _, opt := range opts {
+			opt(&o)
+		}
+
+		return func(ctx context.Context, name string, request any) (response any, err error) {
+			defer func() {
+				if rerr := recover(); rerr != nil {
+					err = o.handler(ctx, name, request, rerr)
+				}
+			}()
+			return next(ctx, name, request)
+		}
+	}
+}
