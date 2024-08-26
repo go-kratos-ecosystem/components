ce403c88ba19002c612b534d7964e0966297b13b
Flc゛ <four_leaf_clover@foxmail.com>
2024-08-26 13:57:49 +0800

    feat(errors): Added TimeoutError
    Signed-off-by: Flc゛ <four_leaf_clover@foxmail.com>

--- errors/timeout_test.go
+++ errors/timeout_test.go
@@ -9,7 +9,9 @@ import (
 
 func TestTimeoutError(t *testing.T) {
 	err := NewTimeoutError(5*time.Second, nil)
-	assert.Equal(t, "Timeout after 5s: <nil>", err.Error())
+	assert.Equal(t, "timeout after 5s: <nil>", err.Error())
 	assert.Nil(t, err.Unwrap())
 	assert.True(t, IsTimeoutError(err))
+	assert.Equal(t, 5*time.Second, err.Timeout)
+	assert.Implements(t, (*error)(nil), err)
 }
