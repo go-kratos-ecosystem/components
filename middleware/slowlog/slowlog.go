b0669e1712af89cace2ba326de5f39b98efbe869
fengchen <fengchenorz@gmail.com>
2024-08-28 09:25:14 +0800

    added ability to record slow log

---
+++ middleware/slowlog/slowlog.go
@@ -0,0 +1,144 @@
+package slowlog
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-kratos/kratos/v2/errors"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware"
+	"github.com/go-kratos/kratos/v2/transport"
+	"time"
+)
+
+// DefaultThreshold Threshold is the slow query threshold, default is 2000ms
+const DefaultThreshold = 2000 * time.Millisecond
+
+type slow struct {
+	Threshold time.Duration
+}
+
+type SlowOption func(*slow)
+
+// Redacter defines how to log an object
+type Redacter interface {
+	Redact() string
+}
+
+// Server returns a middleware that logs slow requests from the server.
+func Server(logger log.Logger, opts ...SlowOption) middleware.Middleware {
+	return func(handler middleware.Handler) middleware.Handler {
+		return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
+
+			o := &slow{
+				Threshold: DefaultThreshold,
+			}
+			for _, opt := range opts {
+				opt(o)
+			}
+
+			var (
+				code      int32
+				reason    string
+				kind      string
+				operation string
+			)
+			startTime := time.Now()
+			duration := time.Since(startTime)
+			if duration <= o.Threshold {
+				return
+			}
+
+			if info, ok := transport.FromServerContext(ctx); ok {
+				kind = info.Kind().String()
+				operation = info.Operation()
+
+			}
+			reply, err = handler(ctx, req)
+			if se := errors.FromError(err); se != nil {
+				code = se.Code
+				reason = se.Reason
+			}
+			level, stack := extractError(err)
+			log.NewHelper(log.WithContext(ctx, logger)).Log(level,
+				"kind", "server",
+				"type", "slowlog",
+				"component", kind,
+				"operation", operation,
+				"args", extractArgs(req),
+				"code", code,
+				"reason", reason,
+				"stack", stack,
+				"latency", time.Since(startTime).Seconds(),
+			)
+			return
+		}
+	}
+}
+
+// Client returns a middleware that logs slow requests from the client.
+func Client(logger log.Logger, opts ...SlowOption) middleware.Middleware {
+	return func(handler middleware.Handler) middleware.Handler {
+		return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
+			var (
+				code      int32
+				reason    string
+				kind      string
+				operation string
+			)
+
+			o := &slow{
+				Threshold: DefaultThreshold,
+			}
+			for _, opt := range opts {
+				opt(o)
+			}
+
+			startTime := time.Now()
+			duration := time.Since(startTime)
+			if duration <= o.Threshold {
+				return
+			}
+			if info, ok := transport.FromClientContext(ctx); ok {
+				kind = info.Kind().String()
+				operation = info.Operation()
+			}
+			reply, err = handler(ctx, req)
+			if se := errors.FromError(err); se != nil {
+				code = se.Code
+				reason = se.Reason
+			}
+			level, stack := extractError(err)
+			log.NewHelper(log.WithContext(ctx, logger)).Log(level,
+				"kind", "client",
+				"type", "slowlog",
+				"component", kind,
+				"operation", operation,
+				"args", extractArgs(req),
+				"code", code,
+				"reason", reason,
+				"stack", stack,
+				"latency", time.Since(startTime).Seconds(),
+			)
+			return
+		}
+	}
+}
+
+// extractError returns the string of the error
+func extractError(err error) (log.Level, string) {
+	if err != nil {
+		return log.LevelError, fmt.Sprintf("%+v", err)
+	}
+	return log.LevelInfo, ""
+}
+
+// extractArgs returns the string of the req
+func extractArgs(req interface{}) string {
+	if redacter, ok := req.(Redacter); ok {
+		return redacter.Redact()
+	}
+	if stringer, ok := req.(fmt.Stringer); ok {
+		return stringer.String()
+	}
+	return fmt.Sprintf("%+v", req)
+}
