c69feac2c7ae3241586e1c13e06f21c418c2fde1
Flc <four_leaf_clover@foxmail.com>
2024-10-18 18:41:33 +0800

    refactor(v2): refactor locker v2

--- locker/v2/redis/locker_test.go
+++ locker/v2/redis/locker_test.go
@@ -19,16 +19,27 @@ func newRedis(t *testing.T) redis.UniversalClient {
 	return rdb
 }
 
-func TestLocker(t *testing.T) {
+func TestLocker_Get(t *testing.T) {
 	rdb := newRedis(t)
 
 	locker := NewLocker(rdb,
-		WithName("locker:test"),
+		WithName("locker:test:get"),
 		WithTTL(time.Second*100),
 	)
 
-	owner, ok, err := locker.Get(context.Background())
+	owner1, ok1, err := locker.Get(context.Background())
 	assert.NoError(t, err)
-	assert.True(t, ok)
-	defer owner.Release(context.Background()) //nolint:errcheck
+	assert.True(t, ok1)
+	assert.NotEmpty(t, owner1.Name())
+
+	_, ok2, err := locker.Get(context.Background())
+	assert.NoError(t, err)
+	assert.False(t, ok2)
+
+	owner1.Release(context.Background()) //nolint:errcheck
+
+	owner3, ok3, err := locker.Get(context.Background())
+	assert.NoError(t, err)
+	t.Logf("owner3: %v, ok3: %v", owner3, ok3)
+	owner3.Release(context.Background()) //nolint:errcheck
 }
