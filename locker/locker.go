ffda29f931ae0b38e0511dfd4cb33f065743cf61
Flc <four_leaf_clover@foxmail.com>
2024-10-22 22:33:55 +0800

    feat(locker): deprecate v1 change to v2

--- locker/locker.go
+++ locker/locker.go
@@ -7,37 +7,42 @@ import (
 )
 
 var (
-	ErrLocked    = errors.New("locker: locked")
-	ErrTimeout   = errors.New("locker: timeout")
-	ErrNotLocked = errors.New("locker: not locked")
+	ErrLocked    = errors.New("locker: the locker is locked")
+	ErrTimeout   = errors.New("locker: the locker is timeout")
+	ErrNotLocked = errors.New("locker: the locker is not locked")
 )
 
 type Locker interface {
-	// Try tries to acquire the lock and execute the function.
-	// If the lock is already acquired, it will return ErrLocked.
-	// Otherwise, it will release the lock after the function is executed, and return the error of the function.
-	Try(ctx context.Context, fn func() error) error
+	// Try tries to get the lock.
+	// if the lock is locked, it will return an error.
+	Try(ctx context.Context, fn func()) error
 
-	// Until tries to acquire the lock and execute the function.
-	// If the lock is already acquired, it will wait until the lock is released or the timeout is reached.
-	// If the timeout is reached, it will return ErrTimeout.
-	// Otherwise, it will release the lock after the function is executed, and return the error of the function.
-	Until(ctx context.Context, timeout time.Duration, fn func() error) error
+	// Until tries to get the lock until the timeout.
+	// if the lock is locked, it will wait until the lock is released or the timeout is reached.
+	Until(ctx context.Context, timeout time.Duration, fn func()) error
 
-	// Release releases the lock for the current owner.
-	// It returns true if the lock is released successfully, otherwise it returns false.
-	// If the lock acquired by another owner, it will return false.
-	Release(ctx context.Context) (bool, error)
+	// Get tries to get the lock.
+	// if the lock is locked, it will return the owner of the lock or an error.
+	Get(ctx context.Context) (Owner, error)
 
-	// ForceRelease releases the lock forcefully.
+	// Release releases the locked owner.
+	// if the owner is not the locked owner, it will return an error.
+	Release(ctx context.Context, owner Owner) error
+
+	// ForceRelease releases the lock forcibly.
 	ForceRelease(ctx context.Context) error
 
-	// Owner return the owner of the lock.
-	Owner() string
-
-	// LockedOwner returns the current owner of the lock.
-	// If the lock is not acquired, it will return an empty string and ErrNotLocked.
-	// Unlike Owner, which returns the owner of the current node in the cluster,
-	// LockedOwner returns the owner of the lock acquired by the Cluster.
-	LockedOwner(ctx context.Context) (string, error)
+	// LockedOwner returns the owner of the lock.
+	LockedOwner(ctx context.Context) (Owner, error)
 }
+
+type NoopLocker struct{}
+
+var _ Locker = NoopLocker{}
+
+func (l NoopLocker) Try(context.Context, func()) error                  { return nil }
+func (l NoopLocker) Until(context.Context, time.Duration, func()) error { return nil }
+func (l NoopLocker) Get(context.Context) (Owner, error)                 { return nil, nil }
+func (l NoopLocker) Release(context.Context, Owner) error               { return nil }
+func (l NoopLocker) ForceRelease(context.Context) error                 { return nil }
+func (l NoopLocker) LockedOwner(context.Context) (Owner, error)         { return nil, nil }
--- locker/redis/locker_test.go
+++ locker/redis/locker_test.go
@@ -8,7 +8,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/google/uuid"
 	"github.com/redis/go-redis/v9"
 	"github.com/stretchr/testify/assert"
 
@@ -17,68 +16,180 @@ import (
 
 var ctx = context.Background()
 
-func newRedisClient() *redis.Client {
-	return redis.NewClient(&redis.Options{
+func newRedis(t *testing.T) redis.UniversalClient {
+	rdb := redis.NewClient(&redis.Options{
 		Addr: "localhost:6379",
 	})
+	t.Cleanup(func() {
+		_ = rdb.Close()
+	})
+	return rdb
 }
 
-func TestLocker(t *testing.T) {
-	client := newRedisClient()
-	var (
-		wg         sync.WaitGroup
-		i          int64
-		try, until atomic.Bool
-		owner      = uuid.New().String()
+func TestLocker_Try(t *testing.T) {
+	l := NewLocker(newRedis(t),
+		WithName("kratos:locker:try"),
+		WithTTL(time.Second*100),
 	)
 
-	redisLocker := NewLocker(
-		client, "locker:test", time.Second*100,
-		WithTimeout(time.Second*100),
-		WithOwner(owner),
-		WithSleep(time.Millisecond*100),
-	)
+	errs := make(chan error, 2)
+	ch := make(chan struct{}, 2)
+	wg := sync.WaitGroup{}
 
-	// Owner
-	assert.Equal(t, owner, redisLocker.Owner())
-
-	// Try
-	wg.Add(10)
-	for j := 0; j < 10; j++ {
+	for i := 0; i < 2; i++ {
+		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			err := redisLocker.Try(ctx, func() error {
-				time.Sleep(time.Millisecond * 500)
-				atomic.AddInt64(&i, 1)
-				return nil
+			errs <- l.Try(ctx, func() {
+				ch <- struct{}{}
+				time.Sleep(time.Millisecond * 10)
 			})
-			if err != nil && errors.Is(err, locker.ErrLocked) {
-				try.Store(true)
-			}
 		}()
 	}
-	wg.Wait()
-	assert.True(t, try.Load())
-	assert.Equal(t, int64(1), i)
 
-	// Until
-	wg.Add(10)
+	wg.Wait()
+	close(errs)
+	close(ch)
+
+	errs1 := <-errs
+	errs2 := <-errs
+	assert.True(t, errs1 == nil || errs2 == nil)
+	assert.False(t, errs1 == nil && errs2 == nil)
+
+	assert.Len(t, ch, 1)
+}
+
+func TestLocker_Until(t *testing.T) {
+	l := NewLocker(newRedis(t),
+		WithName("kratos:locker:until"),
+	)
+
+	ch := make(chan struct{}, 2)
+	wg := sync.WaitGroup{}
 	start := time.Now()
-	for j := 0; j < 10; j++ {
+
+	for i := 0; i < 2; i++ {
+		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			err := redisLocker.Until(ctx, time.Second*5, func() error {
-				time.Sleep(time.Millisecond * 500)
-				atomic.AddInt64(&i, 1)
-				return nil
-			})
-			if err != nil && errors.Is(err, locker.ErrTimeout) {
-				until.Store(true)
-			}
+			assert.NoError(t, l.Until(ctx, time.Second, func() {
+				ch <- struct{}{}
+				time.Sleep(time.Millisecond * 10)
+			}))
 		}()
 	}
+
 	wg.Wait()
-	assert.False(t, until.Load())
-	assert.Equal(t, int64(11), i)
-	assert.True(t, time.Since(start) > time.Second*5)
+	close(ch)
+
+	assert.Len(t, ch, 2)
+	assert.True(t, time.Since(start) > time.Millisecond*10)
+}
+
+func TestLocker_Until_Timeout(t *testing.T) {
+	l := NewLocker(newRedis(t),
+		WithName("kratos:locker:until:timeout"),
+	)
+
+	errs := make(chan error, 2)
+	ch := make(chan struct{}, 2)
+	wg := sync.WaitGroup{}
+
+	for i := 0; i < 2; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			errs <- l.Until(ctx, time.Millisecond*100, func() {
+				ch <- struct{}{}
+				time.Sleep(time.Millisecond * 200)
+			})
+		}()
+	}
+
+	wg.Wait()
+	close(errs)
+	close(ch)
+
+	err1 := <-errs
+	err2 := <-errs
+	assert.True(t, err1 == nil || err2 == nil)
+	assert.False(t, err1 == nil && err2 == nil)
+	assert.True(t, errors.Is(err1, locker.ErrTimeout) || errors.Is(err2, locker.ErrTimeout))
+	assert.False(t, errors.Is(err1, locker.ErrTimeout) && errors.Is(err2, locker.ErrTimeout))
+
+	assert.Len(t, ch, 1)
+}
+
+func TestLocker_GetAndReleaseAndLockedOwner(t *testing.T) {
+	l := NewLocker(newRedis(t),
+		WithName("kratos:locker:release"),
+	)
+
+	owner1, err1 := l.Get(ctx)
+	assert.NoError(t, err1)
+	assert.NotNil(t, owner1)
+	owner, err := l.LockedOwner(ctx)
+	assert.NoError(t, err)
+	assert.NotNil(t, owner)
+	assert.Equal(t, owner1.Name(), owner.Name())
+
+	owner2, err2 := l.Get(ctx)
+	assert.ErrorIs(t, err2, locker.ErrLocked)
+	assert.Nil(t, owner2)
+
+	assert.NoError(t, l.Release(ctx, owner1))
+
+	owner3, err3 := l.Get(ctx)
+	assert.NoError(t, err3)
+	assert.NotNil(t, owner3)
+	assert.NoError(t, owner3.Release(ctx))
+}
+
+func TestLocker_ForceRelease(t *testing.T) {
+	l := NewLocker(newRedis(t),
+		WithName("kratos:locker:force-release"),
+	)
+
+	owner1, err1 := l.Get(ctx)
+	assert.NoError(t, err1)
+	assert.NotNil(t, owner1)
+
+	assert.NoError(t, l.ForceRelease(ctx))
+
+	owner2, err2 := l.Get(ctx)
+	assert.NoError(t, err2)
+	assert.NotNil(t, owner2)
+	assert.NoError(t, owner2.Release(ctx))
+}
+
+// TestLocker_Multi tests the locker with multiple goroutines.
+// see: https://github.com/go-kratos-ecosystem/components/issues/326
+func TestLocker_Multi(t *testing.T) {
+	l := NewLocker(newRedis(t),
+		WithName("kratos:locker:multi"),
+		WithTTL(time.Millisecond*100),
+	)
+
+	ch := make(chan struct{}, 2)
+	wg := sync.WaitGroup{}
+	ii := int64(0)
+
+	for i := 0; i < 2; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if atomic.AddInt64(&ii, 1) == 2 {
+				time.Sleep(time.Millisecond * 105)
+			}
+			assert.NoError(t, l.Try(ctx, func() {
+				ch <- struct{}{}
+				time.Sleep(time.Millisecond * 200)
+			}))
+		}()
+	}
+
+	wg.Wait()
+	close(ch)
+
+	assert.Len(t, ch, 2)
 }
